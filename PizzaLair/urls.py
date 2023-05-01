c21c68e0b8284d1fec8796145c49aa60da3c3392
LightMH <aroningi35@gmail.com>
2023-05-01 13:03:07 +0000

    Pizza ---> menu

--- PizzaLair/urls.py
+++ PizzaLair/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pizza/', include('pizza.urls'))
+    path('menu/', include('menu.urls'))
 ]
--- menu/views.py
+++ menu/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return  HttpResponse('Hello pizza man in index view')
+    return  HttpResponse('Hello menu man in index view')
