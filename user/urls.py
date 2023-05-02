c192971f988d1644cc14f032cd6dc8799f51875c
UnnurOlsen <unnuro22@ru.is>
2023-05-02 15:20:23 +0000

    Vinna í að tengja Búa til aðgang og þá síðu

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="user-index"),
+    path('', views.user_index, name="user-index"),
+    path('', views.new_user_index, name="new_user-index"),
 ]
