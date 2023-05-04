52976cba938f6cd754cdf288d3e2d1e48517ca47
LightMH <aroningi35@gmail.com>
2023-05-04 13:43:13 +0000

    Details site update

--- database_talker.py
+++ database_talker.py
@@ -1,7 +1,11 @@
-from menu.models import Pizza, Toppings, Details, Order, Offers
+from menu.models import Pizza, Toppings, Order, Offers #Details
+
+
 def get_all_pizzas():
     pizza_list = Pizza.objects.all()
     return pizza_list
 
-def get_pizza_by_ID(id):
-    return Pizza.objects.filter()
+
+#def get_pizza_by_id(id):
+ #   print(id)
+  #  return Pizza.(id)
--- menu/urls.py
+++ menu/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name="menu-index"),
-    path('details/', views.details_index, name="details-index")
+    path('<int:id>', views.id_index, name="id-index")
 ]
-
