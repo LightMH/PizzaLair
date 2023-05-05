1679df373eb7295dc3d3898a54808bddb4eec0b0
baraelisabet <70523001+baraelisabet@users.noreply.github.com>
2023-05-05 15:16:43 +0000

    gera toppings (#19)

--- database_talker.py
+++ database_talker.py
@@ -1,14 +1,10 @@
-
-from menu.models import Pizza, Toppings, Order, Offers #Details
+from menu.models import Pizza, Toppings, Order, Offers  # Details
 
 
 def get_all_pizzas():
     pizza_list = Pizza.objects.all()
     return pizza_list
 
-
-
-#def get_pizza_by_id(id):
- #   print(id)
-  #  return Pizza.(id)
-
+# def get_pizza_by_id(id):
+#   print(id)
+#  return Pizza.(id)
