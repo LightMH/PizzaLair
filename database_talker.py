6e28aa11050041a452388b4a522d14279448351f
baraelisabet <bed2@hi.is>
2023-05-05 09:03:57 +0000

    lagaði bara eitthvað smá svo syntax væri pep8

--- database_talker.py
+++ database_talker.py
@@ -1,7 +1,10 @@
 from menu.models import Pizza, Toppings, Details, Order, Offers
+
+
 def get_all_pizzas():
     pizza_list = Pizza.objects.all()
     return pizza_list
 
-def get_pizza_by_ID(id):
+
+def get_pizza_by_id(id):
     return Pizza.objects.filter()
