6397baec6158394f4fea9460f26e66a8d026c1bf
UnnurOlsen <unnuro22@ru.is>
2023-05-05 11:43:34 +0000

    Núna er hægt að leita að ákveðinni pizzu á matseðli, þá er bara sú pizza sýnd á skjánum.

--- menu/views.py
+++ menu/views.py
@@ -1,18 +1,25 @@
-
 from django.shortcuts import render, get_object_or_404
 import database_talker as db_talker
 from menu.models import Pizza
+from django.http import JsonResponse
+
 
 # Create your views here.
 
 
-pizzas = [
-    {"name": "Margarita", "price": "3590 kr"},
-    {"name": "Hawaiian", "price": "3590 kr"}
-]
-
 def index(request):
-    pizza_list = db_talker.get_all_pizzas()
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        pizzas = [{
+            'id': x.id,
+            'name': x.name,
+            'size': x.size,
+            'image': x.image,
+            'type': x.type,
+            'description': x.description
+        } for x in Pizza.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': pizzas})
+    pizza_list = db_talker.get_all_pizzas().order_by('name')
     return render(request, 'menu/menu.html', context={'pizza_list': pizza_list})
 
 
@@ -20,15 +27,13 @@ def details_index(request):
     return render(request, 'menu/details.html')
 
 
-
-#def id_index(request, id):
- #   print(id)
-  #  pizza_id = db_talker.get_pizza_by_id(id)
-   # return render(request, 'menu/details.html', context={'pizza_id': pizza_id})
+# def id_index(request, id):
+#   print(id)
+#  pizza_id = db_talker.get_pizza_by_id(id)
+# return render(request, 'menu/details.html', context={'pizza_id': pizza_id})
 
 def id_index(request, id):
     print(id)
-    return render(request, 'menu/details.html',{
+    return render(request, 'menu/details.html', {
         'pizza': get_object_or_404(Pizza, pk=id)
     })
-
