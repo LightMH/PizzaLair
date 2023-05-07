181d43221d4bbcf9c647e9aeba15e8c956c2fd9a
UnnurOlsen <unnuro22@ru.is>
2023-05-07 21:07:01 +0000

    Bætti við að velja pizzur í hvert tilboð, ekki alveg tilbúið samt sérstakt fyrir hvert og eitt tilboð.

--- offer/urls.py
+++ offer/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="offer-index"),
+    path('<int:id>', views.single_index, name="single-index")
 ]
-
--- offer/views.py
+++ offer/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from offer.models import Offers
+from menu.models import Pizza
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'offer/offer.html')
+    offers = Offers.objects.all()
+    return render(request, 'offer/offer.html', context={'offers': offers})
+
+
+def single_index(request, id):
+    pizza_list = Pizza.objects.all()
+    print(id)
+    return render(request, 'offer/single_offer.html', {
+        'offer': get_object_or_404(Offers, pk=id),
+        'pizzas': pizza_list
+    });
