7106069c0e0042a4ac16da257a79e31d0504b32d
baraelisabet <bed2@hi.is>
2023-05-08 11:51:28 +0000

    Búa til virkni fyrir innskráningu. Nokkur migrations og tvær myndir fyrir matseðil.

--- PizzaLair/settings.py
+++ PizzaLair/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-ra--5(re1x0z=s=44-a&k8nshvbizu0#kra!fl^j=u&+rx-x!f
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PizzaLair.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -89,7 +86,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -109,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -120,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -134,3 +128,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/user/login'
+LOGIN_REDIRECT_URL = "/"
--- user/models.py
+++ user/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User as AuthUser
+
 
 # Create your models here.
 
 
 class CustomerAccount(models.Model):
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
     username = models.CharField(max_length=255, blank=True)
-    password = models.CharField(max_length=255, blank=True)
     phone_number = models.CharField(max_length=8, blank=True)
-    profile_image = models.ImageField(blank=True)
+    profile_image = models.ImageField(max_length=9999, blank=True,
+                                      default="/static/images/profile_icon.png")  # ok þetta er ekki beint að virka
     admin = models.BooleanField(blank=True)
 
 
