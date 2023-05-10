d6003054319473607183eb35659631f6a0f1a190
UnnurOlsen <unnuro22@ru.is>
2023-05-10 12:50:05 +0000

    Reyna að græja tilboð í körfuna

--- static/js/offer.js
+++ static/js/offer.js
@@ -21,9 +21,20 @@ let offer_list = []
 
 let button = document.querySelector(".add-cart");
 button.addEventListener("click", () => {
-    console.log("Button 1 clicked .");
+    // console.log("Button 1 clicked .");
 
-    var valp = document.getElementById('offer.id').innerText ;
+    var valp = document.getElementById('offer.id').innerText;
+
+    //For offers with id 2 og 3, getting the price from the description
+    let offerprice = document.getElementById('offerDescription').innerText;
+    console.log(offerprice)
+
+    //getting the values they choose
+    let values = document.getElementsByClassName('form-select')
+    for (let i = 0; i < values.length; i++) {
+        console.log('Values test', values[i].value)
+        //Just have to send this to the cart
+    }
 
 
     let pizzas = []
@@ -33,12 +44,12 @@ button.addEventListener("click", () => {
     // offerID = offerID.slice(0, - (offerID.length-1))
     // console.log('offer id --> ',offerID)
     // console.log('offer id --> ')
-    console.log(value1)
-    console.log(value2)
-    
+    // console.log(value1)
+    // console.log(value2)
+
 
     let offerName = document.getElementById('offer.name').innerText;
-    
+
     console.log(offerName)
 
     let offer_name = localStorage.getItem('Offer Name');
@@ -48,81 +59,80 @@ button.addEventListener("click", () => {
     curr_price = JSON.parse(curr_price)
     curr_price = parseInt(curr_price)
 
-    let str1 = value1.replace ( /[^\d.]/g, '' );
-    let str2 = value2.replace ( /[^\d.]/g, '' );
+    let str1 = value1.replace(/[^\d.]/g, '');
+    let str2 = value2.replace(/[^\d.]/g, '');
     // let PID1 = str 
-    str1 = str1.slice(0,-1)
-    str2 = str2.slice(0,-1)
-   
-    
+    str1 = str1.slice(0, -1)
+    str2 = str2.slice(0, -1)
 
-    console.log('testing int string2: ',str2)
-    console.log('testing int string1: ',str1)
-      
-    value1 = value1.slice(0,-10)
-    value2 = value2.slice(0,-10)
+
+    console.log('testing int string2: ', str2)
+    console.log('testing int string1: ', str1)
+
+    value1 = value1.slice(0, -10)
+    value2 = value2.slice(0, -10)
     console.log('Testing string', value1)
     console.log('Testing string', value2)
     let str_for1 = ""
     let str_for2 = ""
 
-    for (let i=0; i < 4; i++) { 
+    for (let i = 0; i < 4; i++) {
         let count = 0
         str_for2 += str2[i]
         str_for1 += str1[i]
         count += 1
-        
+
         // if(count == 4) {
         //     str_for2 = parseInt(str2, 10)
         //     str_for1 = parseInt(str1, 10)
         // }
     }
-    console.log("for loop price 1",str_for1)
-    console.log("for loop price 2",str_for2)
+    console.log("for loop price 1", str_for1)
+    console.log("for loop price 2", str_for2)
     // str_for2 = parseInt(str2)
     // str_for1 = parseInt(str1)
-    console.log("for loop price 1",str_for1)
-    console.log("for loop price 2",str_for2)
-    pizzas.push(value1,value2)
+    console.log("for loop price 1", str_for1)
+    console.log("for loop price 2", str_for2)
+    pizzas.push(value1, value2)
     console.log(pizzas)
 
-   let str_int1 = parseInt(str_for1)
-   let str_int2 = parseInt(str_for2)
-   valp = parseInt(valp)
-    if(str_int1 > str_int2) {
-        
+    let str_int1 = parseInt(str_for1)
+    let str_int2 = parseInt(str_for2)
+    valp = parseInt(valp)
+    if (str_int1 > str_int2) {
+
         saveInt = str_int1
     } else {
-        
+
         saveInt = str_int2
-        
-        
+
+
     }
-        // localStorage.setItem("Offers", JSON.stringify(pizzas));
-        // return
-    
-    if(str_for1 > str_for2) {
-        
+    // localStorage.setItem("Offers", JSON.stringify(pizzas));
+    // return
+
+    if (str_for1 > str_for2) {
+
         localStorage.setItem("Offers", JSON.stringify(valp));
     } else {
         localStorage.setItem("Offers", JSON.stringify(valp));
-        
+
     }
 
-    
-    valp = valp * (5**6)
+
+    valp = valp * (5 ** 6)
     console.log("ID int test", valp)
     pr = localStorage.getItem('Offers')
     console.log(pr)
-    
+
     let product = [ // init list með object af pizza id og inCart til að halda um magn af pizzu
-    {
-        ID: valp,
-        Name: pizzas,
-        Price: saveInt,
-        inCart: 0
-    }
-];
+        {
+            ID: valp,
+            Name: pizzas,
+            Price: saveInt,
+            inCart: 0
+        }
+    ];
     setItems(product[0]);
 
 });
@@ -144,7 +154,7 @@ button.addEventListener("click", () => {
 //             Price: valprice,
 //             inCart: 0
 //         }
-        
+
 //     ];
 //     cartNumbers(products[i]);
 //     }
@@ -153,28 +163,27 @@ button.addEventListener("click", () => {
 
 
 // function cartNumbers(product) {  // fara i gegnum localStorage og bæta við 1 ef bætt er i körfu pizzu
-    
+
 //     let productNumbers = localStorage.getItem('cartNumbers');
 
 //     productNumbers = parseInt(productNumbers);
-    
+
 //     if(productNumbers) {
 //         localStorage.setItem('cartNumbers', productNumbers + 1);
-        
+
 //     } else {
 //         localStorage.setItem('cartNumbers', 1);
-        
+
 //     }
 //     setItems(product);
 // }
-
 
 
 function setItems(product) { // setja ID og inCart sem json til að geyma svo hægt sé að sækja í seinna
 
     let productNumbers = localStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
-    if(productNumbers) {
+    if (productNumbers) {
         localStorage.setItem('cartNumbers', productNumbers + 1);
         document.querySelector('.cart span').textContent = productNumbers + 1;
     } else {
@@ -183,18 +192,18 @@ function setItems(product) { // setja ID og inCart sem json til að geyma svo h�
     }
 
     let cartItems = localStorage.getItem('productsInCart'); // sækja Id ..
-    cartItems = JSON.parse(cartItems); 
-    
-    if(cartItems !== null) { 
-        
-        if(cartItems[product.ID] == undefined){
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems !== null) {
+
+        if (cartItems[product.ID] == undefined) {
             cartItems = {
                 ...cartItems, // ... er JS magic for loop
                 [product.ID]: product,
             }
         }
         cartItems[product.ID].inCart += 1
-        if( cartItems[product.ID].inCart == 2) {
+        if (cartItems[product.ID].inCart == 2) {
             cartItems[product.ID].inCart = 2
             // localStorage.setItem('cartNumbers', productNumbers = 2);
             // localStorage.setItem('cartNumbers', productNumbers = 2);
@@ -202,31 +211,26 @@ function setItems(product) { // setja ID og inCart sem json til að geyma svo h�
 
             return
         }
-        
+
     } else {
         product.inCart = 1;
         cartItems = {
-            [product.ID+1]: product
+            [product.ID + 1]: product
         }
     }
     localStorage.setItem("productsInCart", JSON.stringify(cartItems)); // vista i local storage nyja magnið 
-   
+
 }
-
-
-
-
-
 
 
 function empty() {
     //empty cart, refresh siðu 
     console.log("empty")
-    if(localStorage.getItem('Offers')){
+    if (localStorage.getItem('Offers')) {
         localStorage.removeItem('Offers');
-      
-      }
-    
+
+    }
+
 }
 
 
