410aacb9c6c3495b2648465cca4562185b230869
baraelisabet <70523001+baraelisabet@users.noreply.github.com>
2023-05-12 10:30:11 +0000

    Laga payment og stylea checkout phase (#67)

--- cart/forms/payment_form.py
+++ cart/forms/payment_form.py
@@ -4,7 +4,11 @@ CHOICES = [("1", "Borga við afhendingu"), ("2", "Borga með korti")]
 
 
 class PaymentForm(forms.Form):
-    choice_field = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES, required=True)
-    card_holder = forms.CharField(label="Nafn korthafa ", max_length=100, required=True)
-    card_number = forms.CharField(label="Kortanúmer ", max_length=16, required=True)
+    choice_field = forms.ChoiceField(label="", widget=forms.RadioSelect, choices=CHOICES, required=True)
+    card_holder = forms.CharField(label="Nafn korthafa ", max_length=100, required=True,
+                                  error_messages={
+                                      'required': "Vinsamlegast fylltu inn nafn"
+                                  }
+                                  )
+    card_number = forms.CharField(label="Kortanúmer ", min_length=16, max_length=16, required=True)
     cvc = forms.CharField(label="CVC ", max_length=3, min_length=3, required=True)
